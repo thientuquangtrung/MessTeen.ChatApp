08734c3e9632ceef27de7cd6a47b7105055234d0
Thanh Mai <caothithanhmai.911.2017@gmail.com>
2024-01-28 10:14:28 +0700

    fix api list chatroom & add member

--- server/src/v1/modules/ChatRoom/chatroom.route.js
+++ server/src/v1/modules/ChatRoom/chatroom.route.js
@@ -7,7 +7,7 @@ router.get('/', asyncHandler(ChatroomController.list));
 router.post('/create', asyncHandler(ChatroomController.create));
 router.delete('/delete/:roomId', asyncHandler(ChatroomController.delete));
 router.post('/join/:roomId', asyncHandler(ChatroomController.join));
-router.post('/:roomId/leave', asyncHandler(ChatroomController.leave));
+router.post('/leave/:roomId', asyncHandler(ChatroomController.leave));
 router.post('/member/add', asyncHandler(ChatroomController.addMember));
 
 module.exports = router;
