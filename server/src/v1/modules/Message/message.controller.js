9a50b743d67204f642723f8cf9073ceb98c462f9
ducknghia307 <ducnghia30703@gmail.com>
2024-01-29 13:55:33 +0700

    api Message fixing

--- server/src/v1/modules/Message/message.controller.js
+++ server/src/v1/modules/Message/message.controller.js
@@ -24,17 +24,12 @@ class MessageController {
     }).send(res);
   };
 getAllMessage = async (req, res, next) => {
-    try {
         const msg_room_id = req.params.chatroomId.toString();
         const messages = await MessageService.getAllMessages(msg_room_id);
         new OK({
             message: "Get all messages success",
             metadata: messages,
         }).send(res);
-    } catch (error) {
-        // Handle errors appropriately
-        next(error);
-    }
 };
 }
 module.exports = new MessageController();
