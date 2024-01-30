3b25d15767179d32674dd9fea7412a7373e971c3
ducknghia307 <ducnghia30703@gmail.com>
2024-01-30 12:49:54 +0700

    search api chat room

--- server/src/v1/modules/Message/message.controller.js
+++ server/src/v1/modules/Message/message.controller.js
@@ -1,6 +1,6 @@
 const MessageService = require("./message.service");
 const { CREATED, OK } = require("../../core/success.response");
-
+const Chatroom = require("./../ChatRoom/chatroom.model");
 class MessageController {
   send = async (req, res, next) => {
     new CREATED({
@@ -23,13 +23,36 @@ class MessageController {
       metadata: await MessageService.editMessage(messageId, req.body),
     }).send(res);
   };
-getAllMessage = async (req, res, next) => {
-        const msg_room_id = req.params.chatroomId.toString();
-        const messages = await MessageService.getAllMessages(msg_room_id);
-        new OK({
-            message: "Get all messages success",
-            metadata: messages,
-        }).send(res);
-};
+  search = async (req, res, next) => {
+    const msg_room_id = req.params.chatroomId;
+    const msg_content = req.body.msg_content;
+    const chatroom = await Chatroom.findById(msg_room_id);
+
+    if (!chatroom) {
+      throw new NotFoundError("Chatroom not found.");
+    } else if (!msg_content) {
+      return new BadRequest({
+        message: "Empty search content. Please provide a search term.",
+      }).send(res);
+    }
+
+    const messages = await MessageService.searchMessage(
+      msg_room_id,
+      msg_content
+    );
+
+    new OK({
+      message: "Search message success",
+      metadata: messages,
+    }).send(res);
+  };
+  getAllMessage = async (req, res, next) => {
+    const msg_room_id = req.params.chatroomId.toString();
+    const messages = await MessageService.getAllMessages(msg_room_id);
+    new OK({
+      message: "Get all messages success",
+      metadata: messages,
+    }).send(res);
+  };
 }
 module.exports = new MessageController();
