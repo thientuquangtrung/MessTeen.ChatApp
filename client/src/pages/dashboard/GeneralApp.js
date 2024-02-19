0817e955d4ca3ed0b1fc135f96bfa007a468c620
ducknghia307 <107946267+ducknghia307@users.noreply.github.com>
2024-02-19 13:45:31 +0700

    ui chatbox (#36)
    right sidebar of chatroom

--- client/src/pages/dashboard/GeneralApp.js
+++ client/src/pages/dashboard/GeneralApp.js
@@ -1,30 +1,31 @@
-import React from "react";
-import Chats from "./Chats";
-import { Box, Stack, useTheme } from "@mui/material";
+import React from 'react';
+import Chats from './Chats';
+import { Box, Stack, useTheme } from '@mui/material';
 
-import Conversation from "../../components/Conversation";
+import Conversation from '../../components/Conversation';
+import Contact from '../../components/Contact';
 
 const GeneralApp = () => {
-  const theme=useTheme();
+    const theme = useTheme();
 
-  return (
-    <Stack direction={"row"} sx={{ width: "100%" }}>
-      {/* chats */}
-      <Chats />
+    return (
+        <Stack direction={'row'} sx={{ width: '100%' }}>
+            {/* chats */}
+            <Chats />
 
-      <Box
-        sx={{
-          height: "100%",
-          width: "calc(100vw - 420px)",
-          backgroundColor: theme.palette.mode==="light" ? "#fff" : theme.palette.background.paper,
-         
-        }}
-      >
-        {/* conversations */}
-        <Conversation/>
-      </Box>
-    </Stack>
-  );
+            <Box
+                sx={{
+                    height: '100%',
+                    width: 'calc(100vw - 740px)',
+                    backgroundColor: theme.palette.mode === 'light' ? '#fff' : theme.palette.background.paper,
+                }}
+            >
+                {/* conversations */}
+                <Conversation />
+            </Box>
+            <Contact />
+        </Stack>
+    );
 };
 
 export default GeneralApp;
