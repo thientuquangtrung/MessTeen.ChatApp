fd3c320353c344ce5aae461106a5f857d3266db8
Trung Nguyen <thientuquangtrung@gmail.com>
2024-02-24 21:21:12 +0700

    config db uri for in env file

--- client/src/socket.js
+++ client/src/socket.js
@@ -4,7 +4,7 @@ let socket;
 
 const connectSocket = (user_id) => {
     //TODO: fix later
-    socket = io('http://localhost:3051/', {
+    socket = io(process.env.REACT_APP_SERVER_DOMAIN, {
         query: `user_id=${user_id}`,
     });
 }; // Add this -- our server will run on port 3051, so we connect to it from here
