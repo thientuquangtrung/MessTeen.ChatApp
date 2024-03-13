d440fbae934c56a9e741046cda39c4d36b259300
Nguyễn Trịnh Quang Trung <thientuquangtrung@gmail.com>
2024-03-13 12:57:09 +0700

    Feat/admin activity (#86)
    * init admin accoun

--- server/src/v1/modules/User/userStatistic.service.js
+++ server/src/v1/modules/User/userStatistic.service.js
@@ -1,5 +1,6 @@
 const UserStatistic = require('../User/userStatistic.model');
 const { BadRequestError, NotFoundError } = require('../../core/error.response');
+const userModel = require('../User/user.model');
 
 class UserStatisticService {
     static async getUserStatisticByDate(receivedDate) {
@@ -14,8 +15,27 @@ class UserStatisticService {
         return userStatistic;
     }
     static async getAllUserStatistics() {
-        const userStatistics = await UserStatistic.find();
-        return userStatistics;
+        // Step 1: Retrieve all users except admins
+        const users = await userModel.find({ usr_role: 'user' });
+
+        // Step 2: Filter out online users and count them
+        const onlineUsersCount = users.filter((user) => user.usr_status === 'ONLINE').length;
+
+        // Step 3: Count new signups for the current month
+        const startOfMonth = new Date();
+        startOfMonth.setDate(1);
+        startOfMonth.setHours(0, 0, 0, 0);
+
+        const endOfMonth = new Date();
+        endOfMonth.setMonth(endOfMonth.getMonth() + 1, 0);
+        endOfMonth.setHours(23, 59, 59, 999);
+
+        const newSignupUsersCount = await userModel.countDocuments({
+            usr_role: 'user',
+            createdAt: { $gte: startOfMonth, $lte: endOfMonth },
+        });
+
+        return { onlineUsersCount, newSignupUsersCount, totalUsersCount: users.length };
     }
 }
 
